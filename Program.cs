4569b4027f21777f47e43bdf8e7493a809a449f7
Matthew Cavallo <macavall@microsoft.com>
2023-11-23 22:34:24 -0600

    first2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace test
                 .ConfigureFunctionsWorkerDefaults()
                 .Build();
 
-                // test
+                // test 
 
             host.Run();
         }
